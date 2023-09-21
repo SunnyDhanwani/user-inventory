6c0a91d702b8c02b4783d0ae5fbbd966aae9fc52
Sunny Dhanwani <dhanwanisunny678@gmail.com>
2023-09-22 01:30:36 +0530

    Made navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./../styles/Navbar.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="text-white text-24">User&apos;s Inventory</div>
+      <img src="/profile.svg" className="profile-icon" />
     </div>
   );
 };
