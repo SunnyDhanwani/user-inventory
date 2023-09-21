0738013dda2d869e48aaeb76bcf7f10211e313c6
Sunny Dhanwani <dhanwanisunny678@gmail.com>
2023-09-22 01:34:18 +0530

    Fixed navbar font bug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import "./../styles/Navbar.scss";
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <div className="text-white text-24">User&apos;s Inventory</div>
+      <div>User&apos;s Inventory</div>
       <img src="/profile.svg" className="profile-icon" />
     </div>
   );
