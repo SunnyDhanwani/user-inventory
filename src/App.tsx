b7456e30f05d41e7a653e345db63df3a48f6a920
Sunny Dhanwani <sunny.dhanwani@pazago.com>
2023-09-22 10:47:31 +0530

    Fixed modal bugs

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,53 @@ import { getGlobalItem, setGlobalItem } from "./utils/helper";
 import PaginationBar from "./components/PaginationBar";
 import "./styles/App.scss";
 
+const dummyUsers = [
+  {
+    username: "John Doe (Dummy user 1)",
+    dob: "1990-01-01",
+    age: "33",
+    food: "PIZZA",
+    hobbies: "Hacking",
+    gender: "MALE",
+    createdAt: "2023-09-18T17:37:33.256Z",
+    updatedAt: "2023-09-18T17:37:33.256Z",
+    id: 1583472685439,
+  },
+  {
+    username: "Jane Doe (Dummy user 2)",
+    dob: "2000-01-01",
+    age: "23",
+    food: "PIZZA",
+    hobbies: "Singing",
+    gender: "FEMALE",
+    createdAt: "2023-09-19T17:37:33.256Z",
+    updatedAt: "2023-09-19T17:37:33.256Z",
+    id: 1583472685438,
+  },
+  {
+    username: "John Doe (Dummy user 3)",
+    dob: "1994-01-01",
+    age: "29",
+    food: "PIZZA",
+    hobbies: "Music",
+    gender: "MALE",
+    createdAt: "2023-09-20T17:37:33.256Z",
+    updatedAt: "2023-09-20T17:37:33.256Z",
+    id: 1583472685437,
+  },
+  {
+    username: "John Doe (Dummy user 4)",
+    dob: "1960-01-01",
+    age: "63",
+    food: "Burger",
+    hobbies: "Dancing",
+    gender: "MALE",
+    createdAt: "2023-09-21T17:37:33.256Z",
+    updatedAt: "2023-09-21T17:37:33.256Z",
+    id: 1583472685436,
+  },
+];
+
 function App() {
   const [modalDetails, setModalDetails] = useState({
     isOpen: false,
@@ -29,7 +76,7 @@ function App() {
   const handleUserModalOpen = (id: any = "", isView: boolean = false) => {
     if (id) {
       const allUsers = getGlobalItem("allUsersData");
-      const user = allUsers.some((el: any) => {
+      const user = allUsers.find((el: any) => {
         if (el.id === id) return el;
       });
 
@@ -57,61 +104,18 @@ function App() {
     handleUserModalOpen(id);
   };
 
-  // useEffect(() => {
-  //   setGlobalItem("allUsersData", [
-  //     {
-  //       username: "SunnyDhanwani",
-  //       dob: "2018-12-31",
-  //       age: "5",
-  //       food: "PIZZA",
-  //       hobbies: "TS",
-  //       gender: "MALE",
-  //       createdAt: "2023-09-21T17:37:33.256Z",
-  //       updatedAt: "2023-09-21T17:37:33.256Z",
-  //       id: 1583472685439,
-  //     },
-  //     {
-  //       username: "Gudiya",
-  //       dob: "2018-12-31",
-  //       age: "5",
-  //       food: "PIZZA",
-  //       hobbies: "TS",
-  //       gender: "MALE",
-  //       createdAt: "2023-09-21T17:37:33.256Z",
-  //       updatedAt: "2023-09-21T17:37:33.256Z",
-  //       id: 1583472685438,
-  //     },
-  //     {
-  //       username: "Mummy",
-  //       dob: "2018-12-31",
-  //       age: "5",
-  //       food: "PIZZA",
-  //       hobbies: "TS",
-  //       gender: "MALE",
-  //       createdAt: "2023-09-21T17:37:33.256Z",
-  //       updatedAt: "2023-09-21T17:37:33.256Z",
-  //       id: 1583472685437,
-  //     },
-  //     {
-  //       username: "Papa",
-  //       dob: "2018-12-31",
-  //       age: "5",
-  //       food: "PIZZA",
-  //       hobbies: "TS",
-  //       gender: "MALE",
-  //       createdAt: "2023-09-21T17:37:33.256Z",
-  //       updatedAt: "2023-09-21T17:37:33.256Z",
-  //       id: 1583472685436,
-  //     },
-  //   ]);
-  // }, []);
+  useEffect(() => {
+    const users = getGlobalItem("allUsersData");
+
+    if (!users?.length) setGlobalItem("allUsersData", dummyUsers);
+  }, []);
 
   return (
     <div className="App parent-container">
       <UserModal
         isOpen={modalDetails.isOpen}
         handleModalClose={handleUserModalClose}
-        userDetails={modalDetails?.userDetails || undefined}
+        userDetails={modalDetails?.userDetails}
         isView={modalDetails.isView}
       />
 
